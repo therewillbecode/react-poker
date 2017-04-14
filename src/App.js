a41b6f8f568835134ff8c0cd1b23827db6538e33
therewillbecode <tomw08@gmail.com>
2017-04-14 18:27:10 +0100

    Added heading element

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
           <Deck />
         </div>
       </div>
