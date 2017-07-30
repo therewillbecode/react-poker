9bded915c012126e7b9d65660698a1c982b2ebbd
therewillbecode <tomw08@gmail.com>
2017-07-30 12:43:13 +0100

    Added app component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,104 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Deck from './Deck'
+import React, { Component } from "react";
 
-class App extends Component {
+const range = (start, count) =>
+  Array.apply(0, Array(count)).map((element, index) => {
+    return index + start;
+  });
+
+const shuffle = array => {
+  const copy = [];
+  let n = array.length;
+  let i;
+
+  // While there remain elements to shuffle…
+  while (n) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * array.length);
+
+    // If not already shuffled, move it to the new array.
+    if (i in array) {
+      copy.push(array[i]);
+      delete array[i];
+      n--;
+    }
+  }
+
+  return copy;
+};
+
+const getDeck = () =>
+  shuffle(
+    ranks
+      .map(r => suits.map(s => r + s))
+      .reduce((prev, curr) => prev.concat(curr))
+  );
+
+const suits = ["d", "c", "h", "s"];
+const ranks = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { board: [], deck: getDeck() };
+    this.progressDeal = this.progressDeal.bind(this);
+  }
+
+  newRound() {
+    const { deck, board } = this.state;
+    const newDeck = getDeck();
+
+    this.setState(Object.assign({}, { board: [], deck: newDeck }));
+  }
+
+  dealFlop() {
+    const { deck, board } = this.state;
+    const flop = range(0, 3).map(e => deck.pop());
+
+    this.setState(Object.assign({}, { board: flop, deck }));
+  }
+
+  dealCard() {
+    const { deck, board } = this.state;
+    const card = deck.pop();
+
+    this.setState(Object.assign({}, { deck, board: board.concat(card) }));
+  }
+
+  progressDeal() {
+    const { deck, board } = this.state;
+
+    if (board.length === 0) {
+      this.dealFlop();
+      return;
+    }
+    if (board.length === 5) {
+      this.newRound();
+    } else {
+      this.dealCard();
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          <Deck />
-        </div>
+      <div>
+        <button onClick={this.progressDeal} />
       </div>
     );
   }
 }
 
-export default App;
+export default AppContainer;
