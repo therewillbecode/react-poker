ad369d6591b5e6fccd65ae1257315b5384deeefe
therewillbecode <tomw08@gmail.com>
2017-08-26 21:20:14 +0100

    Removed old demo

--- site/react-deck-example/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import AppContainer from "./AppContainer";
-import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<AppContainer />, document.getElementById("root"));
-registerServiceWorker();
