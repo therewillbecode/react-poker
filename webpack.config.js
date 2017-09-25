99a313e706aca7efd5b71095d3a0acd1c904ab94
therewillbecode <tomw08@gmail.com>
2017-09-25 17:01:42 +0100

    Removed stage-0 babel preset

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
         query: {
-          presets: ["es2015", "react", "stage-0"]
+          presets: ["es2015", "react"]
         }
       },
       {
